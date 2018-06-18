907feab4671b1adc793dfaf3b06081314d9ba450
Dejosel <newjos7@gmail.com>
2018-06-17 19:34:25 -0500

    agregando funcion para contar caracteres

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
-      </div>
-    );
-  }
-}
+	constructor(){
+		super()
+		this.state = {
+			leng:0
+		}
+	}
+	counter(event){
+		this.setState({
+			leng: event.target.value.length
+		})
+	}
+	render() {
+		return (
+			<div className="container">
+			<textarea onChange={this.counter.bind(this)} rows="3" ></textarea>
+			<div className="counter">{this.state.leng}</div>
+			</div>
+			);
+		}
+	}
 
-export default App;
+	export default App;
